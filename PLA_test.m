7488192ae67981b7ba514b3c4c82745a834f3753
Steven Janovitz <stevenjanovitz@wustl.edu>
2014-09-14 21:43:48 -0500

    show that found hypothesis does not necessarily equal the real classifying function

--- PLA_test.m
+++ PLA_test.m
@@ -1,12 +1,20 @@
-%Simple implementation of perceptron learning algorithm on random training sets.
+%Simple implementation of perceptron learning algorithm on random,
+%linearly separable training sets.
 %Provides a visualization to see if the number of iterations of the algorithm 
 %is less than the theoretical bound. It is.
+%Shows that there are many weight vectors that generate the same
+%dichotomies, i.e., the hypothesis the algorithm chooses won't necessarily
+%be the same as the real classifying function.
+
 
 iterationsVec = zeros(1,1000);
 tBoundDifferencesVec = zeros(1,1000);
+foundWeightsEqualRealWeightsVec = zeros(1,1000);
 
 for i = 1:1000
     weights = horzcat([0], rand(1,10));
+    realWeights = weights;
+    
     trainingSet = horzcat(ones(100,1),(1 - 2.*rand(100,10)));
     trainingSet = horzcat(trainingSet, transpose(sign(weights * transpose(trainingSet))));
     
@@ -35,8 +43,11 @@ for i = 1:1000
     end
     iterationsVec(i) = iterations;
     tBoundDifferencesVec(i) = t_bound-iterations;
+    
+    foundWeightsEqualRealWeightsVec(i) = (isequal(weights, realWeights));
 end
 
+disp(mean(foundWeightsEqualRealWeightsVec));
 hist(iterationsVec);
 xlabel('Iterations until linear separator is found');
 ylabel('Number of times range of iterations occurs');
